4addf9b65f2573cc601e2ad668194f7d85c36df5
anandromy <romyanand2004@gmail.com>
2025-01-04 10:53:06 +0530

    part1c

--- exercises/part1/introdemo/src/App.jsx
+++ exercises/part1/introdemo/src/App.jsx
@@ -1,56 +1,30 @@
+import { useState } from "react"
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts}/>
-      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text={"Plus"} />
+      <Button onClick={decreaseByOne} text={"Minus"} />
+      <Button onClick={setToZero} text={"Zero"} />
     </div>
   )
 }
 
-const Header = (props) => {
+const Display = ({ counter }) => {
   return (
-    <h1>{props.name}</h1>
+    <div>{counter}</div>
   )
 }
 
-const Content = (props) => {
+const Button = ({ onClick, text }) => {
   return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
--- exercises/part1/introdemo/src/main.jsx
+++ exercises/part1/introdemo/src/main.jsx
@@ -1,4 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App />)
+
